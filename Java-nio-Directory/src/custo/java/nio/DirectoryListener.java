e70f824b2ae6908ad7a440c0bdedec1bdb6117ba
Sephy <nobody@noone.void>
2016-06-24 16:48:13 +0200

    Adding @FonctionnalInterface annotation to DirectoryListener

--- Java-nio-Directory/src/custo/java/nio/DirectoryListener.java
+++ Java-nio-Directory/src/custo/java/nio/DirectoryListener.java
@@ -4,6 +4,7 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.util.List;
 
+@FunctionalInterface
 public interface DirectoryListener {
 	public void onEvent(WatchKey key, List<WatchEvent<?>> events);
 }
